82b9bcc1fb82300beed68aa72ee808bd2496419e
Vasyutchenko Alexey <88984453+ssplexus@users.noreply.github.com>
2021-09-23 12:42:49 +0300

    Change to array

--- src/Alphabet.java
+++ src/Alphabet.java
@@ -2,218 +2,197 @@ class Alphabet{
     private int a, b, c, d, e, f, g, h, i, j, k, l,
             m, n, o, p, q, r, s, t, u, v,
             w, x, y, z, sum;
-    public int getA() {
-        return a;
+    final private int MAX = 26;
+    private int letters_arr[];
+
+    public Alphabet()
+    {
+        letters_arr = new int[MAX];
     }
 
-    public void setA(int a) {
-        this.a = a;
+    public void setLetter(char _letter, int _val)
+    {
+        switch (_letter)
+        {
+            case 'a':
+                letters_arr[0] = _val;
+                break;
+            case 'b':
+                letters_arr[1] = _val;
+                break;
+            case 'c':
+                letters_arr[2] = _val;
+                break;
+            case 'd':
+                letters_arr[3] = _val;
+                break;
+            case 'e':
+                letters_arr[4] = _val;
+                break;
+            case 'f':
+                letters_arr[5] = _val;
+                break;
+            case 'g':
+                letters_arr[6] = _val;
+                break;
+            case 'h':
+                letters_arr[7] = _val;
+                break;
+            case 'i':
+                letters_arr[8] = _val;
+                break;
+            case 'j':
+                letters_arr[9] = _val;
+                break;
+            case 'k':
+                letters_arr[10] = _val;
+                break;
+            case 'l':
+                letters_arr[11] = _val;
+                break;
+            case 'm':
+                letters_arr[12] = _val;
+                break;
+            case 'n':
+                letters_arr[13] = _val;
+                break;
+            case 'o':
+                letters_arr[14] = _val;
+                break;
+            case 'p':
+                letters_arr[15] = _val;
+                break;
+            case 'q':
+                letters_arr[16] = _val;
+                break;
+            case 'r':
+                letters_arr[17] = _val;
+                break;
+            case 's':
+                letters_arr[18] = _val;
+                break;
+            case 't':
+                letters_arr[19] = _val;
+                break;
+            case 'u':
+                letters_arr[20] = _val;
+                break;
+            case 'v':
+                letters_arr[21] = _val;
+                break;
+            case 'w':
+                letters_arr[22] = _val;
+                break;
+            case 'x':
+                letters_arr[23] = _val;
+                break;
+            case 'y':
+                letters_arr[24] = _val;
+                break;
+            case 'z':
+                letters_arr[25] = _val;
+                break;
+            default:
+                System.out.println("Wrong character!");
+        }
     }
 
-    public int getB() {
-        return b;
-    }
-
-    public void setB(int b) {
-        this.b = b;
-    }
-
-    public int getC() {
-        return c;
-    }
-
-    public void setC(int c) {
-        this.c = c;
-    }
-
-    public int getD() {
-        return d;
-    }
-
-    public void setD(int d) {
-        this.d = d;
-    }
-
-    public int getE() {
-        return e;
-    }
-
-    public void setE(int e) {
-        this.e = e;
-    }
-
-    public int getF() {
-        return f;
-    }
-
-    public void setF(int f) {
-        this.f = f;
-    }
-
-    public int getG() {
-        return g;
-    }
-
-    public void setG(int g) {
-        this.g = g;
-    }
-
-    public int getH() {
-        return h;
-    }
-
-    public void setH(int h) {
-        this.h = h;
-    }
-
-    public int getI() {
-        return i;
-    }
-
-    public void setI(int i) {
-        this.i = i;
-    }
-
-    public int getJ() {
-        return j;
-    }
-
-    public void setJ(int j) {
-        this.j = j;
-    }
-
-    public int getK() {
-        return k;
-    }
-
-    public void setK(int k) {
-        this.k = k;
-    }
-
-    public int getL() {
-        return l;
-    }
-
-    public void setL(int l) {
-        this.l = l;
-    }
-
-    public int getM() {
-        return m;
-    }
-
-    public void setM(int m) {
-        this.m = m;
-    }
-
-    public int getN() {
-        return n;
-    }
-
-    public void setN(int n) {
-        this.n = n;
-    }
-
-    public int getO() {
-        return o;
-    }
-
-    public void setO(int o) {
-        this.o = o;
-    }
-
-    public int getP() {
-        return p;
-    }
-
-    public void setP(int p) {
-        this.p = p;
-    }
-
-    public int getQ() {
-        return q;
-    }
-
-    public void setQ(int q) {
-        this.q = q;
-    }
-
-    public int getR() {
-        return r;
-    }
-
-    public void setR(int r) {
-        this.r = r;
-    }
-
-    public int getS() {
-        return s;
-    }
-
-    public void setS(int s) {
-        this.s = s;
-    }
-
-    public int getT() {
-        return t;
-    }
-
-    public void setT(int t) {
-        this.t = t;
-    }
-
-    public int getU() {
-        return u;
-    }
-
-    public void setU(int u) {
-        this.u = u;
-    }
-
-    public int getV() {
-        return v;
-    }
-
-    public void setV(int v) {
-        this.v = v;
-    }
-
-    public int getW() {
-        return w;
-    }
-
-    public void setW(int w) {
-        this.w = w;
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public void setX(int x) {
-        this.x = x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    public void setY(int y) {
-        this.y = y;
-    }
-
-    public int getZ() {
-        return z;
-    }
-
-    public void setZ(int z) {
-        this.z = z;
+    public int getLetter(char _letter)
+    {
+        int val = 0;
+        switch (_letter)
+        {
+            case 'a':
+                val = letters_arr[0];
+                break;
+            case 'b':
+                val = letters_arr[1];
+                break;
+            case 'c':
+                val = letters_arr[2];
+                break;
+            case 'd':
+                val = letters_arr[3];
+                break;
+            case 'e':
+                val = letters_arr[4];
+                break;
+            case 'f':
+                val = letters_arr[5];
+                break;
+            case 'g':
+                val = letters_arr[6];
+                break;
+            case 'h':
+                val = letters_arr[7];
+                break;
+            case 'i':
+                val = letters_arr[8];
+                break;
+            case 'j':
+                val = letters_arr[9];
+                break;
+            case 'k':
+                val = letters_arr[10];
+                break;
+            case 'l':
+                val = letters_arr[11];
+                break;
+            case 'm':
+                val = letters_arr[12];
+                break;
+            case 'n':
+                val = letters_arr[13];
+                break;
+            case 'o':
+                val = letters_arr[14];
+                break;
+            case 'p':
+                val = letters_arr[15];
+                break;
+            case 'q':
+                val = letters_arr[16];
+                break;
+            case 'r':
+                val = letters_arr[17];
+                break;
+            case 's':
+                val = letters_arr[18];
+                break;
+            case 't':
+                val = letters_arr[19];
+                break;
+            case 'u':
+                val = letters_arr[20];
+                break;
+            case 'v':
+                val = letters_arr[21];
+                break;
+            case 'w':
+                val = letters_arr[22];
+                break;
+            case 'x':
+                val = letters_arr[23];
+                break;
+            case 'y':
+                val = letters_arr[24];
+                break;
+            case 'z':
+                val = letters_arr[25];
+                break;
+            default:
+                System.out.println("Wrong character!");
+        }
+        return val;
     }
 
     public int getSum()
     {
-        return a + b + c + d + e + f + g + h + i + j + k + l +
-                m + n + o + p + q + r + s + t + u + v +
-                w + x + y + z;
+        int sum = 0;
+        for (int l: letters_arr)
+        {
+            sum += l;
+        }
+        return  sum;
     }
 }
--- src/main.java
+++ src/main.java
@@ -3,8 +3,9 @@ public class main
     public static void main(String[] args)
     {
         Alphabet alphabet = new Alphabet();
-        alphabet.setA(10);
-        alphabet.setC(15);
+        alphabet.setLetter('a',10);
+        alphabet.setLetter('c',15);
+        System.out.println(alphabet.getLetter('c'));
         System.out.println(alphabet.getSum());
     }
 }
