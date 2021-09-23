d9713806eb983e790fa88a240a7e19ff891e79d6
Vasyutchenko Alexey <88984453+ssplexus@users.noreply.github.com>
2021-09-23 12:50:22 +0300

    Fix main.java

--- src/main.java
+++ src/main.java
@@ -5,6 +5,7 @@ public class main
         Alphabet alphabet = new Alphabet();
         alphabet.setLetter('a',10);
         alphabet.setLetter('c',15);
+        alphabet.setLetter('d',7);
         System.out.println(alphabet.getLetter('c'));
         System.out.println(alphabet.getSum());
     }
