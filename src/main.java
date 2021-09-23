57fff7acb6a78c7a63b1f1ae6ffd615fba2da3ed
Vasyutchenko Alexey <88984453+ssplexus@users.noreply.github.com>
2021-09-23 12:17:44 +0300

    Fix main.java

--- src/main.java
+++ src/main.java
@@ -2,6 +2,9 @@ public class main
 {
     public static void main(String[] args)
     {
-
+        Alphabet alphabet = new Alphabet();
+        alphabet.setA(10);
+        alphabet.setC(15);
+        System.out.println(alphabet.getSum());
     }
 }
