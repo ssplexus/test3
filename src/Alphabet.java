94e26161a512988b1bcda36b013e2670c20b798e
Vasyutchenko Alexey <88984453+ssplexus@users.noreply.github.com>
2021-09-23 13:11:04 +0300

    Alphabet class change

--- src/Alphabet.java
+++ src/Alphabet.java
@@ -1,5 +1,7 @@
 class Alphabet{
-
+    private int a, b, c, d, e, f, g, h, i, j, k, l,
+            m, n, o, p, q, r, s, t, u, v,
+            w, x, y, z, sum;
     final private int MAX = 26;
     private int letters_arr[];
 
